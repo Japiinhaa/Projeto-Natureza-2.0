75bf0f307168d81c75a13416c9341f6411b21408
Vitor Gonçalves <133777140+Japiinhaa@users.noreply.github.com>
2023-10-16 20:22:13 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import accordionInit from "./modules/accordion.js"
 import menuMobileInit from "./modules/menuMobile.js"
 import tabMenuInit from "./modules/tabmenu.js"
+import Formulario from "./modules/modal.js"
 
 tabMenuInit()
 accordionInit()
